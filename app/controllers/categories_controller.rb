13fea46e86d60e676dfbf912f85daecc7dc0719e
Amy <amy@lighthouselabs.ca>
2017-08-31 17:10:42 +0000

    CSS fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,5 @@
 class CategoriesController < ApplicationController
   before_filter :authorize
-  
-  def index
-    @category = Category.all
-  end
-  
-  def new
-    @category = Category.all
-  end
-
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
