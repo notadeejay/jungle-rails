a22f6fa77c268b297150eba1a2cecd94c5b57b0c
Amy <amy@lighthouselabs.ca>
2017-09-04 17:19:38 +0000

    Added user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, uniqueness: true
   has_many :ratings
+
+  validates :email, presence: true
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates :password, presence: true
+  validates_length_of :password, :minimum => 5
+
+  def self.find_by_email_case_insensitive(email)
+    User.where("lower(email) = ?", email.downcase).first
+  end
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email_case_insensitive(email.strip)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'Validations' do  
+  
+  before :each do
+    @user = User.new(name: 'Amy',  email: 'test@gmail.com', password:'password', password_confirmation:'password')
+  end
+  
+  describe 'Validations' do
 
+    it 'password fields should match' do
+      user = User.new(
+        name: 'Amy',
+        email: 'amy@gmail.com',
+        password: '1234',
+        password_confirmation: '123'
+      )
+      expect(user.valid?).to be false
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'email should be unique' do
+      @user.save
+      @user1 = User.new(name: 'Joe', email: 'TEST@GMAIL.COM', password: 'password12', password_confirmation: 'password12')
+      expect(@user1.valid?).to be false
+      expect(@user1.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it 'password should have at least 5 characters' do
+      user = User.new(
+        name: 'Amy',
+        email: 'test@gmail.com',
+        password: 'test',
+        password_confirmation: 'test'
+      )
+      expect(user.valid?).to be false
+      expect(user.errors.full_messages).to include("Password is too short (minimum is 5 characters)")
+    end
+  end
+
+  describe '.authenticate_with_credentials' do  
+    it 'should not care about case when searching by email' do
+      @user.save
+      expect(User.find_by_email_case_insensitive('tEsT@Gmail.com')).to_not eql(nil)
+    end
+    
+    it 'should have authenticated email and password' do
+      @user.save
+      expect(User.authenticate_with_credentials('    test@gmAil.COM', 'password')).to_not eql(nil)
+    end
   end
 end
