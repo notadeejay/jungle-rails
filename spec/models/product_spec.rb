4a95c425b511fdf7e1f67efaf29fc585279f3a6f
Amy <amy@lighthouselabs.ca>
2017-09-04 16:16:14 +0000

    Refactored product validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,59 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
- 
+
+  before :each do
+    @category = Category.new(name: 'Bicyles')
+    @product = Product.new(name: 'Fixie', price_cents: 10000, quantity: 3, category: @category)
+  end
+
   describe 'Validations' do
-      
-    describe 'Product.save' do
-      it 'Should should save' do
-        @category = Category.new( name: 'Test Category' )
-        @product = Product.new(
-          name: 'Test product',
-          price: 150,
-          quantity: 2,
-          category: @category)
-        expect(@product.save).to be true
-      end
+    it 'product should be saved' do
+      @product.save
+    end
+    
+    it 'should validate the presence of name' do
+      @product.name = nil
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
 
-      it 'Name should be present' do
-        @category = Category.new( name: 'Test Category' )
-        @product = Product.create(
-          name: nil,
-          price: 150,
-          quantity: 2,
-          category: @category)
-        expect(@product.errors.full_messages).to include("Name can't be blank")
-      end
+    it 'should validate the presence of price' do
+      @product.price_cents = nil
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include("Price cents is not a number", "Price is not a number", "Price can't be blank")
+    end
 
-      it 'Price should be present' do
-        @category = Category.new( name: 'Test Category' )
-        @product = Product.create(
-          name: 'Test Product',
-          price: nil,
-          quantity: 2,
-          category: @category)
-        expect(@product.errors.full_messages).to include("Price can't be blank")
-      end
+    it 'should validate the presence of quantity' do
+      @product.quantity = nil
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
 
-      it 'Quantity should be present' do
-        @category = Category.new( name: 'Test Category' )
-        @product = Product.create(
-          name: "Test Product",
-          price: 150,
-          quantity: nil,
-          category: @category)
-        expect(@product.errors.full_messages).to include("Quantity can't be blank")
-      end
-
-      it 'Category should be present' do
-        @product = Product.create(
-          name: nil,
-          price: 150,
-          quantity: 2,
-          category: nil)
-        expect(@product.errors.full_messages).to include("Category can't be blank")
-      end
-
+    it 'should validate the presence of category' do
+      @product.category = nil
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
   end
 end
